6c45b261afd2c3223b852e28c7fe44181e2dd8fd
Aayush Tyagi <aayushtyagi3@gmail.com>
2019-07-21 17:48:01 +0530

    Create Find_Seam.hpp

---
+++ Find_Seam.hpp
@@ -0,0 +1,13 @@
+#ifndef FIND_SEAM_H
+#define FIND_SEAM_H
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<vector>
+
+using namespace cv;
+using namespace std;
+
+void FIND_SEAM( Mat& image, vector<int>& path );
+
+#endif
