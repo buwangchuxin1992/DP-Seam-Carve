2c3b695ab8f4f0e3afc2a677d456f52bd6360307
Aayush Tyagi <aayushtyagi3@gmail.com>
2019-07-21 18:14:11 +0530

    Create Energy_Val.hpp

---
+++ Energy_Val.hpp
@@ -0,0 +1,12 @@
+#ifndef ENERGY_VAL_H
+#define ENERGY_VAL_H
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+void GET_ENERGY( Mat& inp, Mat& out );
+
+#endif
