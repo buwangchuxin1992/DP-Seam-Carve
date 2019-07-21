bfb34e9bf11976a6757dbcde21ccb644db88c7d0
Bimal-Sethi <42767796+Bimal-Sethi@users.noreply.github.com>
2019-07-21 20:24:10 +0530

    Update Shrink_Image.cpp

--- Shrink_Image.cpp
+++ Shrink_Image.cpp
@@ -3,10 +3,10 @@
 
 void Shrink_Image(Mat &image, Mat &output, int old_h, int old_w, int new_h, int new_w){
 	output = image;
-	for(int i=0; i< new_w - old_w; i++)
+	for(int i=0; i< old_w - new_w; i++)
 		seam_carve(output);
 	rotate(output,output,ROTATE_90_CLOCKWISE);
-	for(int i=0; i< new_h - old_h; i++)
+	for(int i=0; i< old_h - new_h; i++)
 		seam_carve(output);
 	rotate(output,output,ROTATE_90_COUNTERCLOCKWISE);
 }
