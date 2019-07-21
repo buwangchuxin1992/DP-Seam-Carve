a7514cf96cc44e8a6a4b503effb7a5be70e7519b
Aayush Tyagi <aayushtyagi3@gmail.com>
2019-07-21 19:33:43 +0530

    Update Remove_Seam.cpp

--- Remove_Seam.cpp
+++ Remove_Seam.cpp
@@ -2,7 +2,7 @@
 
 void ERASE_SEAM( Mat& img, Mat& outp, vector<int>& path )
 {
-    int X = img.rows(), Y = img.cols();
+    int X = img.rows, Y = img.cols;
     for(int i=0; i<X; i++) for(int j=0; j<Y-1; j++)
     outp.at<uchar>(i,j) = j<path[i] ? img.at<uchar>(i,j) : img.at<uchar>(i,j+1);
 }
